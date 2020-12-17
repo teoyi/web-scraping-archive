6d1795abbe74d88a063aad3f6e4ac3f4e19fa68e
teoyi <yipengteo@yahoo.com.sg>
2020-12-17 15:09:11 -0800

    initial commit for practicing

---
+++ ws-practice.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://www.pyclass.com/example.html", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+c = r.content # beautiful soup will give us the contents listed here 
+
+soup = BeautifulSoup(c, "html.parser") # need to pass html.parser if not it wont work properly
+print(soup.prettify()) #this will print it out in a html format 
+# viewing this online through inspect will give a clearer idea by highlighting the resepective elements 
