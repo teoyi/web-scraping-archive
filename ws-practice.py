afd7e2f451321f839f615b050b208abb9bede6da
teoyi <yipengteo@yahoo.com.sg>
2020-12-17 15:14:38 -0800

    completed introduction to beautiful soup

--- ws-practice.py
+++ ws-practice.py
@@ -5,5 +5,14 @@ r = requests.get("http://www.pyclass.com/example.html", headers={'User-agent': '
 c = r.content # beautiful soup will give us the contents listed here 
 
 soup = BeautifulSoup(c, "html.parser") # need to pass html.parser if not it wont work properly
-print(soup.prettify()) #this will print it out in a html format 
+#print(soup.prettify()) #this will print it out in a html format 
 # viewing this online through inspect will give a clearer idea by highlighting the resepective elements 
+all = soup.find_all("div",{"class":"cities"})
+#print(all[0]) # indexing is possible for this 
+# if we are looking for specific headers or paragraph 
+#print(all[0].find_all("h2")[0].text) 
+
+for item in all: 
+    print(item.find_all("p")[0].text)
+
+    
