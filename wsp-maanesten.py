85beeb8c3b16af21accd01ee1c0232a19a0a0a97
teoyi <yipengteo@yahoo.com.sg>
2020-12-20 20:26:03 -0800

    Changed duration variable to run for a longer period of time

--- wsp-maanesten.py
+++ wsp-maanesten.py
@@ -66,4 +66,4 @@ if __name__=="__main__":
                          address = address,
                          receiver = receiver,
                          password = password,
-                         run_hours = 1) 
+                         run_hours = 1000) 
