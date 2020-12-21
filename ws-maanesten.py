8c4a3c49e932833f50e3933655b64aee5a50bd97
teoyi <yipengteo@yahoo.com.sg>
2020-12-20 21:43:10 -0800

    Completed webscraping of website with price reporting

--- ws-maanesten.py
+++ ws-maanesten.py
@@ -1,32 +1,15 @@
 import requests
+import re
 import numpy as np
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
-# DRIVER_PATH = '/Users/luke/Downloads/chromedriver'
-# options = Options()
-# options.headless = True
-# options.add_argument("--window-size=1920,1200")
-
-# driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-# driver.get("https://maanesten.com/product-category/accessories-2/hair-claws/")
-
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-# elements = WebDriverWait(driver,30).until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@id='content']")))
-# for element in elements: 
-#     print(element.text)
-
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
+        'Content-Type': 'text/html'}
 gen_url = 'https://maanesten.com/product-category/accessories-2/hair-claws/?metaltype[]=accessories&ajaxcall=1&page='
 pNames = []
 for n in np.arange(1, 7, 1):
     #print(gen_url+str(n))
-    r = requests.get(gen_url+str(n), headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36',
-        'Content-Type': 'text/html'})
+    r = requests.get(gen_url+str(n), headers = header)
     c = r.content 
     soup = BeautifulSoup(c, 'html.parser')
     uls = soup.find('ul', {'class' : 'products'})
@@ -36,10 +19,17 @@ for n in np.arange(1, 7, 1):
 
 for n in np.arange(0, len(pNames)):
     pNames[n] =  pNames[n].replace(' ', '-')
-print(pNames)
+# print(pNames)
 
-
-
-
-
-# driver.quit()
+prod_url = 'https://maanesten.com/product/'
+for n in np.arange(0, len(pNames)):
+    prod_r = requests.get(prod_url+pNames[n], headers = header)
+    prod_c = prod_r.content 
+    prod_s = BeautifulSoup(prod_c, 'html.parser')
+    section = prod_s.find('div', {'class' : 'woocommerce-variation-price'})
+    status = re.findall(r"Out of stock", str(section))
+    amt = prod_s.find('span', {'class' : 'amount'}).text
+    if not status:
+        print(f'{pNames[n]} : IN STOCK')
+    else: 
+        print(f'{pNames[n]} : Out of stock')
